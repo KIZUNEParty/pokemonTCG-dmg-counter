10e57b49d74df0bf47aac0a3bd46a2a9b5357f81
dethMastery <owner@detzz.in.th>
2022-04-26 06:12:16 +0700

    added: footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,9 +6,46 @@
 	<slot />
 </main>
 
+<footer>
+	<span>
+		if u want to donate <a href="https://detzz.in.th/Donation">Click Me</a>
+	</span>
+	<br><br>
+	<span>
+		&copy; 2022 <a href="https://suphakit.net">Suphakit P.</a> All Right Reserved.
+	</span>
+</footer>
+
 <style>
 	main {
 		width: 100%;
 		height: 100vh;
+	}
+
+	footer {
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+
+		background-color: whitesmoke;
+		color: #2e2f2f;
+
+		padding: 1rem;
+		padding-left: 0;
+		padding-right: 0;
+
+		text-align: center;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	footer a {
+		color: #2e2f2f;
+		text-decoration: underline;
+	}
+
+	footer a:hover {
+		color: #2e2f2f;
+		text-decoration: none;
 	}
 </style>
