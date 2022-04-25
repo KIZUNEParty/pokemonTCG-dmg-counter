12529a979dfdb7dc77b590dab22c49095315c8bc
dethMastery <owner@detzz.in.th>
2022-04-25 20:17:11 +0700

    finished: all counter part and gx with v-star

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <script lang="ts">
+	let gxClass :any = ''
+	let vsClass :any = ''
+
 	let active :number = 0;
 	let fst :number = 0;
 	let snd :number = 0;
@@ -26,20 +29,32 @@
 	<div class="dmg-control">
 		<div class="active-pokemon">
 			<div class="gx-vstar">
-				&nbsp;
+				<button class={'GX ' + gxClass} style="width: 13rem;" on:click={e => gxClass = 'activated'}>
+					<h1>
+						GX
+					</h1>
+				</button>
+				<br><br><br>
+				<button class={'v-star ' + vsClass} style="width: 13rem;" on:click={e => vsClass = 'activated'}>
+					<h1>
+						V-Star
+					</h1>
+				</button>
 			</div>
 			<div class="card">
-				<button on:click={e => active = active - 10}>
-					-
-				</button>
-				&nbsp;&nbsp;
-				<span class="dmg active">
-					{active}
-				</span>
-				&nbsp;&nbsp;
-				<button on:click={e => active = active + 10}>
-					+
-				</button>
+				<div class="dmg-container">
+					<button on:click={e => active = active - 10}>
+						-
+					</button>
+					&nbsp;&nbsp;
+					<span class="dmg active">
+						{active}
+					</span>
+					&nbsp;&nbsp;
+					<button on:click={e => active = active + 10}>
+						+
+					</button>
+				</div>
 
 				<br><br>
 
@@ -51,8 +66,117 @@
 				&nbsp;
 			</div>
 		</div>
-		<div class="bench-pokemon">
 
+		<div class="bench-pokemon">
+			<div class="card">
+				<div class="dmg-container">
+					<button on:click={e => fst = fst - 10}>
+						-
+					</button>
+					&nbsp;&nbsp;
+					<span class="dmg fst">
+						{fst}
+					</span>
+					&nbsp;&nbsp;
+					<button on:click={e => fst = fst + 10}>
+						+
+					</button>
+				</div>
+
+				<br><br>
+
+				<button class="setzero" on:click={e=> fst = 0}>
+					Set to '0'
+				</button>
+			</div>
+
+			<div class="card">
+				<div class="dmg-container">
+					<button on:click={e => snd = snd - 10}>
+						-
+					</button>
+					&nbsp;&nbsp;
+					<span class="dmg snd">
+						{snd}
+					</span>
+					&nbsp;&nbsp;
+					<button on:click={e => snd = snd + 10}>
+						+
+					</button>
+				</div>
+
+				<br><br>
+
+				<button class="setzero" on:click={e=> snd = 0}>
+					Set to '0'
+				</button>
+			</div>
+
+			<div class="card">
+				<div class="dmg-container">
+					<button on:click={e => trd = trd - 10}>
+						-
+					</button>
+					&nbsp;&nbsp;
+					<span class="dmg trd">
+						{trd}
+					</span>
+					&nbsp;&nbsp;
+					<button on:click={e => trd = trd + 10}>
+						+
+					</button>
+				</div>
+
+				<br><br>
+
+				<button class="setzero" on:click={e=> active = 0}>
+					Set to '0'
+				</button>
+			</div>
+
+			<div class="card">
+				<div class="dmg-container">
+					<button on:click={e => fth = fth - 10}>
+						-
+					</button>
+					&nbsp;&nbsp;
+					<span class="dmg fth">
+						{fth}
+					</span>
+					&nbsp;&nbsp;
+					<button on:click={e => fth = fth + 10}>
+						+
+					</button>
+				</div>
+
+				<br><br>
+
+				<button class="setzero" on:click={e=> fth = 0}>
+					Set to '0'
+				</button>
+			</div>
+
+			<div class="card">
+				<div class="dmg-container">
+					<button on:click={e => ffth = ffth - 10}>
+						-
+					</button>
+					&nbsp;&nbsp;
+					<span class="dmg ffth">
+						{ffth}
+					</span>
+					&nbsp;&nbsp;
+					<button on:click={e => ffth = ffth + 10}>
+						+
+					</button>
+				</div>
+
+				<br><br>
+
+				<button class="setzero" on:click={e=> ffth = 0}>
+					Set to '0'
+				</button>
+			</div>
 		</div>
 	</div>
 	
@@ -75,8 +199,21 @@
 		height: 100vh;
 	}
 
+	.GX, .v-star {
+		background-color: coral;
+		color: whitesmoke;
+
+		border: none;
+		border-radius: 10px;
+	}
+
+	.activated {
+		opacity: 0.2;
+	}
+
 	.active-pokemon {
 		width: 100%;
+		height: 50vh;
 
 		display: flex;
 		flex-direction: row;
@@ -97,12 +234,34 @@
 		height: auto;
 	}
 
+	.bench-pokemon {
+		width: 100%;
+		height: 50vh;
+
+		display: flex;
+		flex-direction: row;
+
+		text-align: center;
+		vertical-align: middle;
+	}
+
 	.card {
 		width: 60%;
+		height: 30vh;
+
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		vertical-align: middle;
+		align-items: center;
+		align-content: center;
+
 		background-color: grey;
 		padding: 1rem;
 		border-radius: 10px;
 		margin: 1rem;
+
+		vertical-align: middle;
 	}
 
 	.card button {
